764fcf488e1fdbecf11b699665dd9f1e630bdf64
xieyf13 <xyf.713@163.com>
2016-03-07 09:24:31 +0800

    Added files via upload

---
+++ malloc.c
@@ -0,0 +1,126 @@
+/*数据结构block*/
+typedef struct s_block *t_block;
+struct s_block {
+    size_t size;  
+    t_block prev; 
+    t_block next; 
+    int free;     
+    int padding;  
+    void *ptr;    
+    char data[1];
+};
+
+/*查找合适block*/
+t_block find_block(t_block *last, size_t size) {
+    t_block b = first_block;
+    while(b && !(b->free && b->size >= size)) {
+        *last = b;
+        b = b->next;
+    }
+    return b;
+}
+
+
+#define BLOCK_SIZE 24 
+
+/*新建block*/
+t_block extend_heap(t_block last, size_t s) {
+    t_block b;
+    b = sbrk(0);
+    if(sbrk(BLOCK_SIZE + s) == (void *)-1)
+        return NULL;
+    b->size = s;
+    b->next = NULL;
+    if(last)
+        last->next = b;
+    b->free = 0;
+    return b;
+}
+
+/*分裂*/
+void split_block(t_block b, size_t s) {
+    t_block new;
+    new = b->data + s;
+    new->size = b->size - s - BLOCK_SIZE ;
+    new->next = b->next;
+    new->free = 1;
+    b->size = s;
+    b->next = new;
+}
+
+size_t align8(size_t s) {
+    if(s & 0x7 == 0)
+        return s;
+    return ((s >> 3) + 1) << 3;
+}
+
+void *first_block=NULL;
+
+t_block get_block(void *p) {
+    char *tmp;  
+    tmp = p;
+    return (p = tmp -= BLOCK_SIZE);
+}
+ 
+int valid_addr(void *p) {
+    if(first_block) {
+        if(p > first_block && p < sbrk(0)) {
+            return p == (get_block(p))->ptr;
+        }
+    }
+    return 0;
+}
+
+t_block fusion(t_block b) {
+    if (b->next && b->next->free) {
+        b->size += BLOCK_SIZE + b->next->size;
+        b->next = b->next->next;
+        if(b->next)
+            b->next->prev = b;
+    }
+    return b;
+}
+ 
+void *malloc(size_t size) {
+    t_block b, last;
+    size_t s;
+    s = align8(size);
+    if(first_block) {
+        last = first_block;
+        b = find_block(&last, s);
+        if(b) {
+            if ((b->size - s) >= ( BLOCK_SIZE + 8))
+                split_block(b, s);
+            b->free = 0;
+        } else {
+            b = extend_heap(last, s);
+            if(!b)
+                return NULL;
+        }
+    } else {
+        b = extend_heap(NULL, s);
+        if(!b)
+            return NULL;
+        first_block = b;
+    }
+    return b->data;
+}
+
+void free(void *p) {
+    t_block b;
+    if(valid_addr(p)) {
+        b = get_block(p);
+        b->free = 1;
+        if(b->prev && b->prev->free)
+            b = fusion(b->prev);
+        if(b->next)
+            fusion(b);
+        else {
+            if(b->prev)
+                b->prev->prev = NULL;
+            else
+                first_block = NULL;
+            brk(b);
+        }
+    }
+}
